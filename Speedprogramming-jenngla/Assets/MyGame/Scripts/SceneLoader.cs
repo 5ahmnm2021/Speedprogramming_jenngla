3721edf74270617fcc051a9a60db5ba50cb3fd1d
jenngla <jennifer.glantschnig@htl-salzburg.ac.at>
2021-04-13 09:40:51 +0200

    Scene3 and SceneLoader were added

---
+++ Speedprogramming-jenngla/Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void Load01()
+    {
+        // if correct Button was pressed, this will happen
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void Load02()
+    {
+        // if correct Button was pressed, this will happen
+        SceneManager.LoadScene("Scene3");
+    }
+}
